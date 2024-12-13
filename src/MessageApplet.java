4a94dfe947fa94b519220d107fd152d46031809e
SuvodeepGuria <suvodeepguria5@gmailcom>
2024-12-13 23:09:11 +0530

    commited

--- src/MessageApplet.java
+++ src/MessageApplet.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+//import java.applet.Applet;
+import java.applet.Applet;
+import java.awt.Graphics;
 
-public class MessageApplet {
+public class MessageApplet extends Applet {
+    @Override
+    public void paint(Graphics g) {
+        // Display a message in the applet window
+        g.drawString("Hello, Welcome to Java Applet!", 50, 100);
+    }
 }
--- src/ThreadExample.java
+++ src/ThreadExample.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
 
 public class ThreadExample {
+    public static void main(String[] args) {
+        MyRunnable myRunnable = new MyRunnable();
+        Thread thread = new Thread(myRunnable);
+        thread.start();
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Main thread is running: " + i);
+            try {
+                Thread.sleep(500); // Pause for 0.5 seconds
+            } catch (InterruptedException e) {
+                System.out.println("Main thread interrupted: " + e.getMessage());
+            }
+        }
+    }
+}
+class MyRunnable implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Thread is running: " + i);
+            try {
+                Thread.sleep(1000); // Pause for 1 second
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted: " + e.getMessage());
+            }
+        }
+    }
 }
